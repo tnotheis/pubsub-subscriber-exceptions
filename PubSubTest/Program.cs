711f3d817b5c462e564aeb4f4c048206dd5d86ea
Timo Notheisen <timo.notheisen@js-soft.com>
2023-09-06 08:10:57 +0200

    init

---
+++ PubSubTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.PubSub.V1;
+
+const string projectId = "<...>";
+const string subscriptionId = "<...>";
+var credentials = GoogleCredential.FromJson("<...>");
+
+var subscriberClient = new SubscriberClientBuilder
+{
+    GoogleCredential = credentials,
+    SubscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId)
+}.Build();
+
+await subscriberClient.StartAsync((_, _) => Task.FromResult(SubscriberClient.Reply.Ack));
